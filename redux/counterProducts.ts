b3e96d550891826d668411d7e9ce904a3829486b
Patrick Tavares <patricktavares3pc@gmail.com>
2022-08-25 14:25:12 -0300

    main: criando o store e o reducer do redux

---
+++ redux/counterProducts.ts
@@ -0,0 +1,26 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+interface CounterState {
+  value: number;
+}
+
+const initialState: CounterState = {
+  value: 0,
+};
+
+export const counterSlice = createSlice({
+  name: "counterCar",
+  initialState,
+  reducers: {
+    increment: (state) => {
+      state.value++;
+    },
+    decrement: (state) => {
+      state.value--;
+    },
+  },
+});
+
+export const { increment, decrement } = counterSlice.actions;
+
+export default counterSlice.reducer;
